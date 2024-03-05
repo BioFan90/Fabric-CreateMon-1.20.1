c10d3bbd52ff18031a7281b3c00c5719474e66b1
Brandon O <32948553+BioFan90@users.noreply.github.com>
2024-03-04 21:39:52 -0500

    Initial commit

---
+++ src/main/java/net/biofan/cobblecreate/CreatemonClient.java
@@ -0,0 +1,10 @@
+package net.biofan.cobblecreate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreatemonClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/biofan/cobblecreate/CreatemonDataGenerator.java
@@ -0,0 +1,11 @@
+package net.biofan.cobblecreate;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreatemonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
